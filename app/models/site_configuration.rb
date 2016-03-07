5e81a600ff0f08d4b472878bdff0782888e08b53
Edwin Chow <echow6@gmail.com>
2016-03-07 16:29:10 -0500

    done

--- app/models/site_configuration.rb
+++ app/models/site_configuration.rb
@@ -1,7 +1,7 @@
 class SiteConfiguration < ActiveRecord::Base
   validates :site_name, presence: true
   validates :url, presence: true, url: true
-  validates :variations, presence: true, inclusion: 1..100
+  validates :variations, inclusion: 1..100
   validates :campaign_delimiter, presence: { message: "can't be empty if 'Enable Origin Experiments' is selected"}, if: :origin_experiments_checked?
   validates :factor_delimiter, presence: { message: "can't be empty if 'Enable Origin Experiments' is selected"}, if: :origin_experiments_checked?
 
